8263869159a263d939315c5f8e863aecdf354075
pacheco55 <pachecoyeah55@outlok.com>
2025-02-12 03:05:34 -0600

    En esta priemra version de implementacion de API se obtiene por .json para desserializar la lista de los topicos  e interactuar con la base de datos para gregar mas dispositivos , no se perueba pero no marca error el codigo .

--- DUALAPP/postlogin.xaml.cs
+++ DUALAPP/postlogin.xaml.cs
@@ -1,3 +1,8 @@
+using System.Collections.ObjectModel;
+using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 namespace DUALAPP
 {
     public partial class postlogin : ContentPage
@@ -5,14 +10,53 @@ namespace DUALAPP
         public postlogin()
         {
             InitializeComponent();
+            Devices = new ObservableCollection<Device>();
+            DevicesCollectionView.ItemsSource = Devices;
+            //LoadDevicesFromApi();
         }
 
-      
+
         private async void Backlogin(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new MainPage());
         }
-       
 
+        public ObservableCollection<Device> Devices { get; set; }
+
+        private async void LoadDevicesFromApi()
+        {
+            // Lógica para cargar dispositivos desde la API
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetStringAsync("https://api.example.com/devices");
+                var devices = JsonSerializer.Deserialize<List<Device>>(response);
+                foreach (var device in devices)
+                {
+                    Devices.Add(device);
+                }
+            }
+       }
+
+
+        private void OnAddDevice(object sender, EventArgs e)
+        {
+            // Lógica para agregar un nuevo dispositivo
+            Devices.Add(new Device { Name = "Nuevo Dispositivo" });
+        }
+
+        private void OnDeleteDevice(object sender, EventArgs e)
+        {
+            // Lógica para eliminar un dispositivo
+            var button = sender as Button;
+            var device = button.BindingContext as Device;
+            Devices.Remove(device);
+        }
+
+        public class Device
+        {
+            public string Name { get; set; }
+            public string Temperature { get; set; }
+            public string Humidity { get; set; }
+        }
     }
 }
