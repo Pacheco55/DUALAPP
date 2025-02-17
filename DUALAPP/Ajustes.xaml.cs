26f2ab66f925714d3d54e557cd9180d7ae7670ac
pacheco55 <pachecoyeah55@outlok.com>
2025-02-16 23:20:19 -0600

    Se agrega AUDIO de fondo en toda la app en general aun no sirven los controles de la pagina de AJUSTES pero no marca error el codigo como aqui en el push , tiene la pagina de ejemplos de graficas c0pn Microcharts.Maui .

--- DUALAPP/Ajustes.xaml.cs
+++ DUALAPP/Ajustes.xaml.cs
@@ -1,10 +1,17 @@
+using Microsoft.Maui.Controls;
+using Plugin.Maui.Audio;
+
 namespace DUALAPP
 {
     public partial class Ajustes : ContentPage
     {
+        private IAudioPlayer _audioPlayer;
         public Ajustes()
         {
             InitializeComponent();
+            volumeSlider.ValueChanged += OnVolumeChanged;
+            soundSwitch.Toggled += OnSoundToggled;
+
         }
 
         async private void OnSaveSettings(object sender, EventArgs e)
@@ -19,5 +26,24 @@ namespace DUALAPP
             // Lógica para regresar a la página postlogin
             await Navigation.PushAsync(new postlogin());
         }
+
+        private void OnVolumeChanged(object sender, ValueChangedEventArgs e)
+        {
+            _audioPlayer.Volume = e.NewValue / 100;
+        }
+
+        private void OnSoundToggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+            {
+                _audioPlayer.Play();
+            }
+            else
+            {
+                _audioPlayer.Pause();
+            }
+        }
+
+        
     }
 }
--- DUALAPP/App.xaml.cs
+++ DUALAPP/App.xaml.cs
@@ -1,12 +1,24 @@
-﻿namespace DUALAPP
+﻿using Plugin.Maui.Audio;
+
+namespace DUALAPP
 {
     public partial class App : Application
     {
+        private IAudioPlayer _audioPlayer;
+
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
+
+            InitializeAudioPlayer();
+        }
+
+        private async void InitializeAudioPlayer()
+        {
+            var audioStream = await FileSystem.OpenAppPackageFileAsync("CelloSuite1.mp3");
+            _audioPlayer = AudioManager.Current.CreatePlayer(audioStream);
+            _audioPlayer.Play();
         }
     }
 }
